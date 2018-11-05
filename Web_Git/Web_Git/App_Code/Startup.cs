1079339e37fd47421e5edea47449d5a66111bbe6
fashraf186 <fashraf@xavor.com>
2018-11-05 16:02:03 +0500

    New file added

---
+++ Web_Git/Web_Git/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_Git.Startup))]
+namespace Web_Git
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
